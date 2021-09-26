f8a541ee486e3e2250be0a364fc6f7c8bbb93cea
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-09-26 11:25:01 -0700

    Weather info now renders.

--- src/Weather.js
+++ src/Weather.js
@@ -10,13 +10,13 @@ export default class Weather extends React.Component {
           return(
             <Card key={index} style={{ width: '20rem' }}>
               <Card.Body>
-                <Card.Title>{day.valid_date}</Card.Title>
+                <Card.Title>{day.day}</Card.Title>
                 <Card.Text>{day.description}</Card.Text>
               </Card.Body>
             </Card>
           )
         }
-        )}
+      )}
       </Container>
     )
   }
